5ec0a1bfb5e6674dce0f19fecbeffd1a1ac4ba37
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-03-11 15:06:49 +0600

    Update customer-add.php

--- InvoiceMg-PHP/customer-add.php
+++ InvoiceMg-PHP/customer-add.php
@@ -6,7 +6,7 @@ include('header.php');
 
 <h1>Add Customer</h1>
 <hr>
-
+ 
 <div id="response" class="alert alert-success" style="display:none;">
 	<a href="#" class="close" data-dismiss="alert">&times;</a>
 	<div class="message"></div>
@@ -99,4 +99,5 @@ include('header.php');
 
 <?php
 	include('footer.php');
+
 ?>
