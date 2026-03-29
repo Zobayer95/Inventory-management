a4eec87e6efb4dd75fa32df2e4c544ec8b962bca
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-03-29 16:03:42 +0600

    Update customer-add.php

--- InvoiceMg-PHP/customer-add.php
+++ InvoiceMg-PHP/customer-add.php
@@ -4,7 +4,7 @@ include('header.php');
  
 ?>       
       
-<h1>Add Customer</h1>    
+<h1>Add Customer</h1>     
 <hr>          
      
 <div id="response" class="alert alert-success" style="display:none;"> 
