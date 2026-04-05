b758cc5628c6a48deae04066b63e8fb1df4b8304
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-04-05 12:37:48 +0600

    Update customer-add.php

--- InvoiceMg-PHP/customer-add.php
+++ InvoiceMg-PHP/customer-add.php
@@ -8,7 +8,7 @@ include('header.php');
           
 <div id="response" class="alert alert-success" style="display:none;">      
 	<a href="#" class="close" data-dismiss="alert">&times;</a>    
-	<div class="message"></div>
+	<div class="message"></div> 
 </div>
 
 <form method="post" id="create_customer">  
